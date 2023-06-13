2c832448dbedefd31d78b18a5e60005248e7131e
Nate Korous <nathaniel.korous@gmail.com>
2023-06-13 08:03:13 -0600

    init commit, basic scaffold

---
+++ server.ts
@@ -0,0 +1,54 @@
+import * as Phaser from 'phaser'
+
+const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
+    active: false,
+    visible: false,
+    key: 'Game'
+}
+
+const CANVAS_WIDTH = 720
+const CANVAS_HEIGHT = 528
+
+export class GameScene extends Phaser.Scene {
+    constructor() {
+        super(sceneConfig)
+    }
+
+    public create() {
+        const cloudCityTilemap = this.make.tilemap({ key: 'cloud-city-map' })
+        cloudCityTilemap.addTilesetImage('Cloud City', 'tiles')
+
+        for (let i = 0; i < cloudCityTilemap.layers.length; i++) {
+            const layer = cloudCityTilemap.createLayer(i, 'Cloud City', 0, 0)
+            if (layer) {
+                layer.setDepth(i)
+                layer.scale = 3
+            }
+        }
+    }
+
+    public update() { }
+
+    public preload() {
+        this.load.image('tiles', './assets/cloud_tileset.png')
+        this.load.tilemapTiledJSON('cloud-city-map', './assets/cloud_city_map.json')
+    }
+}
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    title: 'Phaser 2d RPG',
+    render: {
+        antialias: false
+    },
+    type: Phaser.AUTO,
+    scene: GameScene,
+    scale: {
+        width: CANVAS_WIDTH,
+        height: CANVAS_HEIGHT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
+    parent: 'game',
+    backgroundColor: '#48C4F8'
+}
+
+export const game = new Phaser.Game(gameConfig)
---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require("path");
+const webpack = require("webpack");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        app: "./server.ts",
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+
+    output: {
+        filename: "app.js",
+        path: path.resolve(__dirname, "dist")
+    },
+
+    mode: "development",
+
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, "index.html").replace(/\\/g, '/'),
+                    to: path.resolve(__dirname, "dist").replace(/\\/g, '/')
+                },
+                {
+                    from: path.resolve(__dirname, "assets", "**", "*").replace(/\\/g, '/'),
+                    to: path.resolve(__dirname, "dist").replace(/\\/g, '/')
+                }
+            ]
+        })
+    ],
+
+};
